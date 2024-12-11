a99ad9926b399b3ab58da91b28893a1e0ec8756f
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-12 00:15:46 +0530

    que

---
+++ 1. Event Loop/120_Quest.js
@@ -0,0 +1,19 @@
+// Recursive setTimeout
+
+setTimeout(() => {
+  console.log("Timeout 1");
+  setTimeout(() => console.log("Timeout 2"), 0);
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log("Promise 1");
+  setTimeout(() => {
+    console.log("Timeout 3");
+    Promise.resolve().then(() => console.log("Promise 2"));
+  }, 0);
+});
+
+
+// Promises execute (Promise 1), then Timeout 1, Timeout 3, Promise 2, Timeout 2.
+
+// Output: Promise 1, Timeout 1, Timeout 3, Promise 2, Timeout 2.
