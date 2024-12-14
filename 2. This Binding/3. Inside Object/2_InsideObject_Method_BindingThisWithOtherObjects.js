3c05f077437f99f11559551b403d327be477fe06
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-14 16:01:42 +0530

    bind

---
+++ 2. This Binding/3. Inside Object/2_InsideObject_Method_BindingThisWithOtherObjects.js
@@ -0,0 +1,14 @@
+const obj1 = {
+  name: 'Aman',
+  getName: function () {
+    return this.name;
+  },
+};
+
+const obj2 = { name: 'Flipkart' };
+
+console.log(obj1.getName.call(obj2));
+
+// Output: 'Flipkart'
+
+// Explanation: Using call, we explicitly bind this to obj2.
