c887fb763f3da7a526ade174dc944cf9d8b7c6cd
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-14 15:21:01 +0530

    browser

--- 2. This Binding/2. Inside Function/1_NonStrict.js
+++ 2. This Binding/2. Inside Function/1_NonStrict.js
@@ -6,6 +6,6 @@ function test() {
 
 // Output:
 
-// In browsers: Window object.
-// In Node.js: global object.
+// In browsers: Window object (Propertise like Audio Settings, GPU texture, Navigator).
+// In Node.js: global object ( Properties like setTimeout, setInterval, fetch).
 // Explanation: In a regular function, this points to the global object in non-strict mode.
