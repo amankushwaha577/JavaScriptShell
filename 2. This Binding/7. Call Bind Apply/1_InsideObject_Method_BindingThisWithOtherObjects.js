31a4c993aa707e161f48249645b8c5da99833fde
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-14 19:56:08 +0530

    call apply

---
+++ 2. This Binding/7. Call Bind Apply/1_InsideObject_Method_BindingThisWithOtherObjects.js
@@ -0,0 +1,18 @@
+const obj1 = {
+  name: 'Aman',
+  sname:'kushwaha',
+  getName: function () {
+    return this.name + " "+ this.sname;
+  },
+};
+
+const obj2 = { name: 'Flipkart' };
+
+console.log(obj1.getName.call(obj2));
+
+// Output: "Flipkart undefined"
+
+// Key Concepts:
+// 1. call allows you to explicitly set the value of this.
+// 2. If the object provided to call does not have the expected properties, 
+//    accessing those properties will result in undefined.
---
+++ 2. This Binding/7. Call Bind Apply/2_InsideObject_Method_BindingThisWithOtherObjects.js
@@ -0,0 +1,18 @@
+const obj1 = {
+  name: 'Aman',
+  getName: function () {
+    return this.name;
+  },
+};
+
+const obj2 = { name: 'Flipkart' };
+
+// Use apply to call obj1's getName method with obj2 as this
+console.log(obj1.getName.apply(obj2));
+
+// Output: "Flipkart undefined"
+
+// Comparison Between call and apply:
+// call: Pass arguments individually: func.call(thisArg, arg1, arg2, ...).
+// apply: Pass arguments as an array: func.apply(thisArg, [arg1, arg2, ...]).
+// In this case, since there are no arguments passed to getName, call and apply work identically.
