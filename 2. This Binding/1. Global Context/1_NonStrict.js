ac39b86105f8e78ebc5642a1e8cf267cad442fe2
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-14 21:18:09 +0530

    strict

--- 2. This Binding/1. Global Context/1_NonStrict.js
+++ 2. This Binding/1. Global Context/1_NonStrict.js
@@ -3,7 +3,7 @@ console.log(this);
 // Output:
 
 // 1. In browsers: Window object (Propertise like Audio Settings, GPU texture, Navigator).
-// 2. In Node.js: {} (empty object).
+// 2. In Node.js:  Global object ({} (empty object)).
 
 //    Explanation: In the global execution context, this refers to the global object (window in browsers and global in Node.js). 
 //    In modules (Node.js), global object is an empty object.
