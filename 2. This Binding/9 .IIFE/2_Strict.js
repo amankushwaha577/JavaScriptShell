7b387273d909dbd43c2e87903e513b49796754e6
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-15 15:31:22 +0530

    iifee

--- 2. This Binding/9 .IIFE/2_Strict.js
+++ 2. This Binding/9 .IIFE/2_Strict.js
@@ -8,15 +8,16 @@
 // In Node.js: undefined
 
 // Explanation:
-// 1. Strict Mode Changes the Value of this:
-// In non-strict mode, this inside a function (including an IIFE) defaults to the global object:
-// In browsers, this is the window object.
-// In Node.js, this is the global object (global).
-// In strict mode, this inside a regular function (or IIFE) defaults to undefined because strict mode enforces a stricter set of rules for this behavior.
+// 1. In non-strict mode, this inside a function (including an IIFE) defaults to the global object:
+//    In browsers, this is the window object.
+//    In Node.js, this is the global object (global).
+// 
 
 // 2. Why undefined in Strict Mode?
-// Strict mode eliminates the implicit binding of this to the global object.
-// Instead, if this is not explicitly set, it remains undefined.
+// When you execute an IIFE, it’s treated like a regular function.
+// In strict mode, if a function is not called with an explicit this value, this defaults to undefined.
+// In strict mode, this inside a regular function (or IIFE) defaults to undefined because strict mode enforces a stricter set of rules for this behavior.
+// Strict mode eliminates the implicit binding of this to the global object in functions( including iife).
 
 // 3. Important Notes:
 // This behavior applies only to functions. Arrow functions behave differently because they do not have their own this.
