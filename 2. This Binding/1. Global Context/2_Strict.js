2475a9d763c64669fc03e09725d39a4699e9c86c
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-15 12:47:15 +0530

    strict

--- 2. This Binding/1. Global Context/2_Strict.js
+++ 2. This Binding/1. Global Context/2_Strict.js
@@ -5,7 +5,7 @@ console.log(this);
 
 
 // 1. In browsers: undefined.
-// 2. In Node.js: {} (empty object) (aka Global object).
+// 2. In Node.js: {} (empty object) (aka object below Global object).
 
 
 // 1. In Browsers:
