518234676ac6f77f3f8e9cae212d15f6046146e0
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-17 23:16:09 +0530

    proto

---
+++ 3. Prototype and Inheritance/1_Prototype/2_Prototype.js
@@ -0,0 +1,37 @@
+let arr1= ["Aman", "kushwaha"]
+
+// 1. If you do arr1. =>
+// ---------------------------------------------------------------- 
+//    => You will get every single property of Array ( of Only Array )
+//       like .length, .map(), .filter(), .reduce()
+//    => At the bottom of all elements you will get [[Prototype]]
+//       all those properties of Array will also be here ( Of Only Array )
+
+// 2. If you do arr1.__proto__ =>
+// ----------------------------------------------------------------
+//    => You will get all those ( Of Only Array ) properties in array form.
+//       [.length, .map(), .filter(), .reduce(), ......]
+
+
+
+
+let obj1 = {
+    name : "Aman",
+    city: "Gurgaon",
+    getInfo : function (){
+        console.log(this.name + " from " + this.city);
+    }
+}
+
+// 1. If you do obj1. =>
+// ---------------------------------------------------------------- 
+//    => You will get every single property of Object ( of Only Object )
+//       like .valueOf(), .toString(), .hasOwnProperty(), .isPrototypeOf()
+//    => At the bottom of all elements you will get [[Prototype]]
+//       all those properties of Object will also be here ( Of Only Object )
+
+// 2. If you do arr1.__proto__ =>
+// ----------------------------------------------------------------
+//    => You will get all those ( Of Only Object ) properties in array form.
+//       [.valueOf(), .toString(), .hasOwnProperty(), .isPrototypeOf() ......]
+
