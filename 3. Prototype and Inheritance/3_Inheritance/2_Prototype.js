6250036145fb8116ba39ab8ffe0c68189d731ca4
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-19 22:57:37 +0530

    proto

--- 3. Prototype and Inheritance/3_Inheritance/2_Prototype.js
+++ 3. Prototype and Inheritance/3_Inheritance/2_Prototype.js
@@ -12,7 +12,9 @@ fun.__proto__.myBind() // Hi Aman
 // To access that method in our __proto__ of function
 
 fun.__proto__
-// Actually function dont gave properties in __proto__
+// Actually function dont gives properties in __proto__
+// so we will get op like -
 // op: ƒ () { [native code] }
 
 fun.myBind() // Hi Aman
+// first it will try to acess __proto__ of fun. and here itself myBind is there so just stop here
