111aedfea8ff67d506f3d296c96bae130246e5c4
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-21 02:45:44 +0530

    proto

--- 3. Prototype and Inheritance/5_Interview/112_constructorProperty.js
+++ 3. Prototype and Inheritance/5_Interview/112_constructorProperty.js
@@ -7,5 +7,5 @@ Person.prototype = {};
 
 console.log(john.constructor === Person); // false
 
-// 1. The constructor points to the function used to create an object.
+// 1. The constructor property points to the function that is used to create an object.
 // 2. Overwriting the prototype breaks the link to the constructor.
