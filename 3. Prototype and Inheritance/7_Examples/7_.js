7b033eade85379cd1db81b95a203de01f6c2249f
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-20 17:55:12 +0530

    proto

--- 3. Prototype and Inheritance/7_Examples/7_.js
+++ 3. Prototype and Inheritance/7_Examples/7_.js
@@ -2,4 +2,9 @@ const obj = {};
 const animal = { eats: true };
 
 Object.setPrototypeOf(obj, animal);
+// obj.__proto__ = animal
+
+console.log(obj.eats);  // true
+
 console.log(Object.getPrototypeOf(obj) === animal); // true
+// console.log(obj.__proto__ === animal);
