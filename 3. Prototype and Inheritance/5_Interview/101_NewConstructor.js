470fcee1303223da9932dd19356624ad3c4f6a9f
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-21 01:34:21 +0530

    new

--- 3. Prototype and Inheritance/5_Interview/101_NewConstructor.js
+++ 3. Prototype and Inheritance/5_Interview/101_NewConstructor.js
@@ -2,6 +2,11 @@ function Person() {}
 console.log(Person.prototype); // Op : {}
 // Every custom function / Constructor in JavaScript has a prototype property by default, which is an empty object ({}).
 
+// But other datatype custom variables gives undefined.
+let a = [1,2,3]  // a.prototype => undefined
+let b= {a:"a"}   // a.prototype => undefined
+let c = "a"      // a.prototype => undefined
+
 
 console.log(new Person().__proto__); // Same as Person.prototype
 // Op : {}
@@ -11,3 +16,7 @@ console.log(new Person().__proto__); // Same as Person.prototype
 
 console.log(Person.prototype === new Person().__proto__); // true
 // Prototype of custom Function === __proto__ of newly created instance 
+
+
+console.log(Person.__proto__ === Function.prototype) // true
+// __proto__ of custom Function === prototype of Function constructor
