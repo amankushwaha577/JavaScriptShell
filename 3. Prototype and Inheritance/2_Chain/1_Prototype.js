66b53b65d14cbb844e9f00334b7000f09791ffd3
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-20 15:34:12 +0530

    proto

--- 3. Prototype and Inheritance/2_Chain/1_Prototype.js
+++ 3. Prototype and Inheritance/2_Chain/1_Prototype.js
@@ -54,3 +54,25 @@ console.log(multiplyBy5.power); // 2
 console.log(multiplyBy5.prototype); // op: {} // It's "this" of multiplyBy5.prototype 
 console.log(arr1.prototype); // undefined
 console.log(user.prototype); // undefined
+
+// --------------------Amazing About Functions------------------------
+// 1. For functions in JavaScript, the prototype property is used when the function is intended to be used as a constructor. 
+//    It allows you to define properties and methods that will be inherited by objects created via the new keyword
+
+// 2. Every custom function in JavaScript has a prototype property by default, which is an empty object ({}).
+// 3. This prototype object is used to define properties or methods for instances if the function is used as a constructor.
+
+      multiplyBy5.prototype.description = "Multiplies a number by 5";
+      console.log(multiplyBy5.prototype); 
+//    Output: { description: 'Multiplies a number by 5' }
+
+
+// 1. arr1.prototype is undefined because custom arrays do not have a prototype property directly; they inherit from Array.prototype.
+      console.log(arr1.prototype);
+//    Output: undefined
+
+      console.log(Object.getPrototypeOf(arr1) === Array.prototype); // true
+      console.log(arr1.__proto__ === Array.prototype); // true
+
+// -------------------------------------------------------------------
+
