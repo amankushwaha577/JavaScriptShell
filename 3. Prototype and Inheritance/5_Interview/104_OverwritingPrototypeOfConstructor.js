df349b59cf04236ac29970d099022c1cf352122e
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-21 02:02:44 +0530

    overwritting

---
+++ 3. Prototype and Inheritance/5_Interview/104_OverwritingPrototypeOfConstructor.js
@@ -0,0 +1,21 @@
+function Person(name) {
+    this.name = name;
+  }
+  
+  Person.prototype = { age: 30 };
+  
+  const john = new Person("John");
+  
+  console.log(john.age); // 30
+
+
+// Explanation:
+// ---------------
+// Person.prototype was replaced with a new object ({ age: 30 }).
+// New instances like john inherit from the new prototype.
+
+
+console.log(Person.prototype); // Op : { age: 30 }
+console.log(john.__proto__); // Op : { age: 30 }
+
+  
