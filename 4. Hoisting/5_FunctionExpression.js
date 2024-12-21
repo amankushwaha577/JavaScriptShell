279b5f3c78e70f63a0d909f5fbd923540f2abf12
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-21 16:03:14 +0530

    comb

---
+++ 4. Hoisting/5_FunctionExpression.js
@@ -0,0 +1,30 @@
+console.log(add(2, 3));
+console.log(sub(5, 3));
+
+function add(a, b) {
+  return a + b;
+}
+
+var sub = function (a, b) {
+  return a - b;
+};
+
+// op: 
+// 5
+// TypeError: sub is not a function
+
+// Explanation:
+// 1. add is a function declaration and is fully hoisted.
+// 2. sub is a function expression assigned to a variable. The variable sub is hoisted, but the function itself is not, so sub is undefined at the time of the call.
+
+
+
+// Hoisting with Arrow Functions:
+// ------------------------------
+// Arrow functions are treated like function expressions.
+console.log(sum(2, 3));
+var sum = (a, b) => a + b;
+
+// OP: TypeError: sum is not a function
+
+
