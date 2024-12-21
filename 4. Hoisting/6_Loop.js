385d1b35ebe69d5b0d491864d2445af2df362f12
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-21 16:06:38 +0530

    lopp

---
+++ 4. Hoisting/6_Loop.js
@@ -0,0 +1,27 @@
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 0);
+}
+
+// 0
+// 1
+// 2
+
+// Explanation:
+// Each iteration of the loop creates a new block-scoped i due to let, 
+// and the value is preserved for each callback in setTimeout.
+
+
+
+
+// If var were used:
+// ----------------------------
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 0);
+}
+// 3
+// 3
+// 3
+
+// Explanation:
+// var is function-scoped, so the same i is shared across all iterations. 
+// By the time the setTimeout callbacks run, the loop has already completed, and i is 3.
