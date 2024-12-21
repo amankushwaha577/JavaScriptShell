d4943d7288a087260e0ddbfc7bed58704490dc71
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-21 16:50:45 +0530

    loop

--- 4. Hoisting/6_Loop.js
+++ 4. Hoisting/6_Loop.js
@@ -7,6 +7,7 @@ for (let i = 0; i < 3; i++) {
 // 2
 
 // Explanation:
+// let is block-scoped, 
 // Each iteration of the loop creates a new block-scoped i due to let, 
 // and the value is preserved for each callback in setTimeout.
 
