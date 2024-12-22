8ed40f1c0a50f456b4d23305e2d7ce890c48616f
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-22 23:52:07 +0530

    console.log(new String("test") == "test");  // true console.log(new String("test") === "test"); // false

--- 12. Equality and Comparisons/1_Basic.js
+++ 12. Equality and Comparisons/1_Basic.js
@@ -153,3 +153,45 @@ console.log(true == "true"); // false
 
 // true == "true":
 // "true" is a string and cannot be coerced into 1, so the result is false.
+
+
+
+
+// 10. Objects Converted to Strings
+const obj = { a: 1 };
+console.log(obj == "[object Object]");  // true
+
+// Explanation:
+// When comparing an object to a string, the object is coerced to its string representation using .toString(), 
+// which for most objects is "[object Object]".
+
+
+
+
+// 11. Comparing Infinity
+console.log(Infinity == Infinity);  // true
+console.log(Infinity === Infinity); // true
+
+// Explanation:
+// Infinity is a special numeric value that is equal to itself, even in strict equality.
+
+
+
+
+// 12. +0 and -0
+console.log(+0 === -0);  // true
+console.log(Object.is(+0, -0)); // false
+
+// Explanation:
+// +0 === -0:
+// In JavaScript, +0 and -0 are considered equal in == and ===.
+
+// Object.is(+0, -0):
+// Object.is is stricter and can distinguish between +0 and -0.
+
+
+
+
+// 13. Primitive Wrappers
+console.log(new String("test") == "test");  // true
+console.log(new String("test") === "test"); // false
