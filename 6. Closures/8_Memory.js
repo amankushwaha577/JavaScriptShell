8120c9bcebd789587978947ac9dcf303903b0182
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-22 11:38:21 +0530

    memory

---
+++ 6. Closures/8_Memory.js
@@ -0,0 +1,18 @@
+function outer() {
+  let outerVar = "Outer variable";
+
+  return function inner() {
+    console.log(outerVar);
+  };
+}
+
+const closureFunc1 = outer();
+const closureFunc2 = outer();
+closureFunc1(); // "Outer variable"
+closureFunc2(); // "Outer variable"
+
+
+// Explanation:
+
+// Even though outer() is called twice, each returned closure retains access to its own outerVar in memory, 
+// which can be useful for managing state in a functional style.
