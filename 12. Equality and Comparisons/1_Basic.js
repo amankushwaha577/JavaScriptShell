8c787ca2b695b5d23bed8585fcf8f9d3bda2dfa3
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-22 23:30:23 +0530

    basics

---
+++ 12. Equality and Comparisons/1_Basic.js
@@ -0,0 +1,38 @@
+// 1. Difference Between == and ===
+console.log(1 == "1");   // true
+console.log(1 === "1");  // false
+
+// Explanation:
+// == (Loose Equality):
+// -----------------------
+// Converts both values to the same type (type coercion).
+// "1" (string) is converted to 1 (number), and the comparison becomes 1 == 1, which is true.
+
+// === (Strict Equality):
+// ------------------------
+// Compares values without any type conversion.
+// 1 (number) is not equal to "1" (string), so the result is false.
+
+
+
+// 2. null and undefined Comparisons
+console.log(null == undefined);  // true // important **
+console.log(null === undefined); // false
+console.log(null == 0);          // false
+
+// Explanation:
+// null == undefined:
+// ------------------
+// null and undefined are loosely equal due to special rules in ==.
+// "Note" ==> According to JavaScript's specification, null is loosely equal to undefined. This is a special exception for these two values.
+// "No type conversion is needed in this case".
+
+// null === undefined:
+// -------------------
+// The === operator is a strict equality check that does not allow type coercion.
+// Strict equality checks type and value.
+// Since null and undefined are different types, the result is false.
+
+// null == 0:
+// --------------
+// "Note" ==> According to JavaScript's specification, null is only loosely equal to undefined and no other value (not even 0).
