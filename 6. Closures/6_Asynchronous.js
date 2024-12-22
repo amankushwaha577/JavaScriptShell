c3135fa02e23c050bf8e49e3d6ec85bc6c5c3f4a
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-22 11:34:51 +0530

    async

---
+++ 6. Closures/6_Asynchronous.js
@@ -0,0 +1,15 @@
+function delayedMessage() {
+  let message = "Hello, after 2 seconds!";
+  setTimeout(function() {
+    console.log(message);
+  }, 2000);
+}
+
+delayedMessage(); // "Hello, after 2 seconds!"
+
+
+// Explanation:
+
+// The setTimeout function retains access to the message variable from the outer function's scope, 
+// even though the function has already finished execution. 
+// This is a closure in action with asynchronous code.
