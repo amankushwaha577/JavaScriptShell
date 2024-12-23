421b507f2a621a19f5295f053430a4c964d6c24a
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-23 12:45:10 +0530

    forin

--- 15. Mix/2_For-in For-of.js
+++ 15. Mix/2_For-in For-of.js
@@ -1,18 +1,3 @@
-// 1. typeof Quirks :
-// The typeof operator returns a string indicating the type of its operand. While straightforward, it has quirks:
-
-console.log(typeof null); // "object" (quirk)
-console.log(typeof []); // "object"
-console.log(typeof function () {}); // "function"
-console.log(typeof NaN); // "number" (quirk)
-
-// Key Notes:
-// A. null returns "object" because of a historical bug in JavaScript.
-// B. Arrays return "object", not "array". Use Array.isArray([]) for arrays.
-// C. typeof NaN is "number", though it means "Not-a-Number".
-
-
-
 // 2. for...in vs for...of
 // for...in: Iterates over enumerable keys of an object or array.
 // for...of: Iterates over values of an iterable (e.g., arrays, strings, maps).
