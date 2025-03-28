bab87481d18f970716b3fb341bb69541cda62b69
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-03-29 00:28:01 +0530

    hosting

--- 1. GEC & Event Loop/FAANG/1. Phases Of EventLoop/1. setTimeout().js
+++
@@ -1,7 +0,0 @@
-/*
-setTimeout() :
--------------
-
-setTimeout() is a function that schedules a callback to be executed after a specified delay in milliseconds. 
-The minimum delay is approximately 4ms in modern browsers and Node.js.
-*/
