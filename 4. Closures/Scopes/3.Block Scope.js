cc16438eda0285132250801d6ed53d8e35dc7dd2
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-03-29 21:52:28 +0530

    scope

--- 4. Closures/Scopes/3.Block Scope.js
+++ 4. Closures/Scopes/3.Block Scope.js
@@ -5,10 +5,27 @@ let x = 10;
 }
 console.log(x); // ?
 
-// Answer:
-// ---------------------
-// var x = 20: SyntaxError
-// let creates a block-scoped variable, and you cannot redeclare x with var in the same scope.
+/*
+OP : SyntaxError
+
+a. let x = 10;
+   x is declared using let, which is block-scoped. And It's in global scope.
+
+b. var x = 20;
+   var is function-scoped. so It does't care about block {}. so It's also in global scope.
+
+   But let x = 10; already exists in the global scope, and var cannot redeclare a let variable in the same scope.
+*/
+
+
+let y = 10;
+{
+  let y = 20;  // Different block-scoped variable
+  console.log(y); // 20 (block scope)
+}
+console.log(y); // 10 (outer scope)
+
+// Solution : because the inner y is scoped only inside the block.
 
 
 /*
