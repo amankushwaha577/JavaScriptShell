f6bdb70bf01970b8d786ca04bbd8be1223cfc31a
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-03-30 02:42:56 +0530

    init

--- 6. This Binding/7 .IIFE/1_NonStrict.js
+++ 6. This Binding/7 .IIFE/1_NonStrict.js
@@ -3,30 +3,31 @@
   })();
   
 
-//   Output:
+// Output:
 
 // In browsers: Window object.
-// In Node.js: {} (empty object).
+// In Node.js:  {} (empty object).
 // Explanation: In a non-strict IIFE, this refers to the global object.
 
 
+/*
+The output of the IIFE (Immediately Invoked Function Expression) in Node.js is {} ( module.exports).
+Not the global object is due to the way Node.js handles the module wrapper function.
 
-// ------------------------------------------------------------------------------------
-// The output of the IIFE (Immediately Invoked Function Expression) in Node.js as {} (empty object || module.exports) rather than
-//  the global object  is due to the way Node.js handles the module wrapper function.
+(function (exports, require, module, __filename, __dirname) {
+           (function () {
+                 console.log(this); // {} (empty object)
+           })();
+});
 
-// (function (exports, require, module, __filename, __dirname) {
-//            (function () {
-//                  console.log(this); // {} (empty object)
-//            })();
-// });
+1. Scope Inside the Wrapper Function:
+------------------------------------
+Inside the wrapper function, the value of this is set to module.exports (an empty object {} by default).
+This means at the top level of a module, this refers to module.exports.
 
+2. Inside the IIFE:
+------------------
+"The IIFE is executed within the module's context, not the global context". ((make it note))
+Since this in the module context is initialized as module.exports (or {}), the this inside the IIFE inherits the same value.
 
-
-// 1. Scope Inside the Wrapper Function:
-// Inside the wrapper function, the value of this is set to module.exports (an empty object {} by default).
-// This means at the top level of a module, this refers to module.exports.
-
-// 2. Inside the IIFE:
-// """""""""""The IIFE is executed within the module's context, not the global context""""""""""". ((make it note))
-// Since this in the module context is initialized as module.exports (or {}), the this inside the IIFE inherits the same value.
+*/
--- 6. This Binding/7 .IIFE/2_Strict.js
+++ 6. This Binding/7 .IIFE/2_Strict.js
@@ -7,22 +7,15 @@
 // In browsers: undefined
 // In Node.js: undefined
 
-// Explanation:
-// 1. In non-strict mode, this inside a function (including an IIFE) defaults to the global object:
-//    In browsers, this is the window object.
-//    In Node.js, this is the global object (global).
-// 
+/*
+1. Why undefined in Strict Mode?
+-------------------------------
+a. When you execute an IIFE, it’s treated like a regular function.
+   In strict mode, if a function is not called with an explicit this value, this defaults to undefined.
 
-// 2. Why undefined in Strict Mode?
-// When you execute an IIFE, it’s treated like a regular function.
-// In strict mode, if a function is not called with an explicit this value, this defaults to undefined.
-// In strict mode, this inside a regular function (or IIFE) defaults to undefined because strict mode enforces a stricter set of rules for this behavior.
-// Strict mode eliminates the implicit binding of this to the global object in functions( including iife).
-
-// 3. Important Notes:
-// This behavior applies only to functions. Arrow functions behave differently because they do not have their own this.
-
-
+b. Important Notes: This behavior applies only to functions. 
+                    Arrow functions behave differently because they do not have their own this.
+*/
 
 
 ( () => {  // Gloal context
