d5e9bad66f783022a8af7c9248b4b54fed245a35
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-03-29 18:35:57 +0530

    compar

--- 3. Data Types & Type Coercion/2. Implicit & Explicit Type Coercion.js/3. Comparisons with==and===.js
+++ 3. Data Types & Type Coercion/2. Implicit & Explicit Type Coercion.js/3. Comparisons with==and===.js
@@ -123,7 +123,7 @@ console.log(null === undefined); // Output: false
 
 
 
-// 5. Comparing Arrays :
+// 6. Comparing Arrays :
 // ---------------------
    const arr1 = [1, 2, 3];
    const arr2 = [1, 2, 3];
@@ -194,5 +194,57 @@ console.log(null === undefined); // Output: false
 
 
 
-console.log(Boolean(0));  // Output: false
-console.log(Boolean(""));  // Output: false
+// 8. Comparing Functions :
+// -------------------------
+   function a() {}
+   function b() {}
+
+   console.log(a == b);   // false
+   console.log(a === b);  // false
+
+   // Explanation:
+   // Functions, like objects, are compared by reference.
+   // a and b are two different instances, so they are not equal.
+
+
+
+// 9. Objects Converted to Strings :
+// ---------------------------------
+   const obj = { a: 1 };
+   console.log(obj == "[object Object]");  // true
+
+   // Explanation:
+   // When comparing an object to a string, the object is coerced to its string representation using .toString(), 
+   // which for most objects is "[object Object]".
+
+
+
+
+
+// 10. +0 and -0
+   console.log(+0 === -0);  // true
+   console.log(Object.is(+0, -0)); // false
+
+   // Explanation:
+   // +0 === -0:
+   // In JavaScript, +0 and -0 are considered equal in == and ===.
+
+   // Object.is(+0, -0):
+   // Object.is is stricter and can distinguish between +0 and -0.
+
+
+
+
+// 11. Primitive Wrappers :
+// -----------------------
+   console.log(new String("test") == "test");  // true
+   console.log(new String("test") === "test"); // false
+
+   // Explanation:
+   // ==: The string object is coerced to its primitive value ("test"), so the comparison is true.
+   // ===: Strict equality fails because one is an object and the other is a string.
+
+
+
+   console.log(Boolean(0));  // Output: false
+   console.log(Boolean(""));  // Output: false
