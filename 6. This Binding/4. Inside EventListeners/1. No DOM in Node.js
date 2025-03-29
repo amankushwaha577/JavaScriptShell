eab7980c9825f18f685dd2e6c3bcc9512b5568ce
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-03-30 01:39:03 +0530

    init

--- 6. This Binding/4. Inside EventListeners/1. No DOM in Node.js
+++ 6. This Binding/4. Inside EventListeners/1. No DOM in Node.js
@@ -7,8 +7,6 @@ document.body.appendChild(button);
 
 
 // The error : document is not defined occurs because the document object is not available in Node.js by default—it is specific to web browsers. 
-// In a Node.js environment, there is no DOM (Document Object Model), 
-// so document and window are undefined unless you're using a library like JSDOM or running the code in a browser-like environment.
 
 // Why This Happens?
 // Browser Context: The document object is provided by the browser to interact with the DOM.
