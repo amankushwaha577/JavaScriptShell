b9c257732fe1f6101ac55a73d000100d7852471d
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-03-30 13:45:55 +0530

    in

--- 7. Prototype and Inheritance/3. New Keyword/FAANG/1. New Instance From Constructor.js
+++ 7. Prototype and Inheritance/3. New Keyword/FAANG/1. New Instance From Constructor.js
@@ -3,4 +3,4 @@ const dog = new Animal();
 
 console.log(dog.__proto__ === Animal.prototype); // true
 
-// __proto__ of new instance's is set to the prototype of the constructor that created it.
+// __proto__ of new instance's is set to the prototype of the CreateUser() constructor that created it.
--- 7. Prototype and Inheritance/3. New Keyword/FAANG/2. New Instance From Constructor.js
+++ 7. Prototype and Inheritance/3. New Keyword/FAANG/2. New Instance From Constructor.js
@@ -1,6 +1,6 @@
 function Person() {}
 console.log(Person.prototype); // Op : {}
-// Every custom function / Constructor in JavaScript has a prototype property by default, which is an empty object ({}).
+// Every custom function / Constructor in JavaScript has a prototype by default, which is an empty object ({}).
 
 // But other datatype custom variables gives undefined.
 let a = [1,2,3]  // a.prototype => undefined
@@ -10,18 +10,15 @@ let c = "a"      // a.prototype => undefined
 
 console.log(new Person().__proto__); // Same as Person.prototype
 // Op : {}
-// Every custom function in JavaScript has a prototype property by default, which is an empty object ({}).
-// It is just like "newlyCreatedInstancefromPerson.__proto__"
-
-
-console.log(Person.prototype === new Person().__proto__); // true
-// Prototype of custom Function === __proto__ of newly created instance
-
+// newly created instance inherits that prototype from Constructor using new Keyword.
 
 
 // Think Like This :
 console.log(new Person().__proto__ === Person.prototype ); // true
+// __proto__ of new instance's is set to the prototype of the CreateUser() constructor that created it.
+
                   // Compare ⬇️
+                  
 // But Note :
 console.log(Person.__proto__ === Function.prototype) // true
 // __proto__ of custom Function === prototype of Function constructor
