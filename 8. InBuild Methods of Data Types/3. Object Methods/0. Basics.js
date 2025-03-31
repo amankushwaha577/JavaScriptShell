90225a308bac3732bdbbc54e7594d5f5e3f51300
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-04-01 00:21:30 +0530

    init

---
+++ 8. InBuild Methods of Data Types/3. Object Methods/0. Basics.js
@@ -0,0 +1,35 @@
+/*
+Que🔹. What are the different types of object built-in methods in JavaScript?
+Answer. JavaScript provides various object methods that can be categorized as follows:
+
+1. Object Creation & Prototyping Methods:
+-----------------------------------------
+   a. Object.create(proto) → Creates a new object with the given prototype.
+   b. Object.assign(target, ...sources) → Copies properties from source objects to target.
+
+2. Object Property Methods:
+---------------------------
+   a. Object.keys(obj) → Returns an array of property keys.
+   b. Object.values(obj) → Returns an array of property values.
+   c. Object.entries(obj) → Returns an array of key-value pairs.
+   d. Object.hasOwn(obj, prop) → Checks if an object has a specific property.
+
+3. Object Freezing & Sealing Methods:
+-------------------------------------
+   a. Object.freeze(obj) → Makes an object immutable.
+   b. Object.seal(obj) → Prevents adding/removing properties but allows modification.
+
+4. Object Descriptor & Prototype Methods:
+-----------------------------------------
+   a. Object.getOwnPropertyDescriptor(obj, prop) → Returns property descriptor.
+   b. Object.getOwnPropertyDescriptors(obj) → Returns all descriptors.
+   c. Object.getPrototypeOf(obj) → Returns the prototype of an object.
+   d. Object.setPrototypeOf(obj, proto) → Sets the prototype of an object.
+
+5. Object Cloning & Merging Methods:
+-------------------------------------
+   a. Object.fromEntries(entries) → Creates an object from key-value pairs.
+   b. Object.assign(target, ...sources) → Merges objects into a target.
+   c. StructuredClone(obj) → Deep copies an object.
+*/
+
