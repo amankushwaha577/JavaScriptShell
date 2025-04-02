d4235c53aa10a5c89355df3c055a5b756c8b9add
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-04-03 00:31:27 +0530

    basics

--- 8. InBuild Methods of Data Types/2. Array Methods/0. Basics.js
+++ 8. InBuild Methods of Data Types/2. Array Methods/0. Basics.js
@@ -4,30 +4,32 @@ Answer. JavaScript provides various array methods that can be categorized as fol
 
 1. Mutation Methods (Modify Original Array):
 --------------------------------------------
-   a. push(), pop(), shift(), unshift(),     => Insert | Delete
-   b. splice(),                              => Insert | Delete | Update
+   a. push(), pop(), shift(), unshift(),     => Insert | Delete          at either Start or end.
+   b. splice(),                              => Insert | Delete | Update at anywhere using start index
    c. reverse(), sort(), fill(), 
 
 
 2. Non-Mutation Methods (Return a New Array) :
 ---------------------------------------------
-    a. map(), filter(), slice(), 
-    b. concat(), 
-    c. flat(), flatMap()
+    a. map(), filter(), 
+    b. slice(),            => Slices array using Start Index.
+       concat(),           => Merges Arrays
+    c. flat(),             => Flats the array (Default 1 level)
+       flatMap()           => Flats the array after performing user actions ( .map().flat(1) )
 
 
 3. Iteration Methods :
 ---------------------
     a. forEach(), map(), filter(), 
-    b. some(), every(), 
-    c. reduce(), reduceRight()
+    b. some(), every(),              => returns true/false
+    c. reduce(), reduceRight()       => returns single value
 
 
 4. Search Methods :
 -------------------
-    a. find(), findIndex(), 
-    b. indexOf(), lastIndexOf(), 
-    c. includes()
+    a. find(), findIndex(),        => searches on the basis of condition
+    b. indexOf(), lastIndexOf(),   => searches on the basis of element
+    c. includes()                  => Just checks ( true / false )
 
     
 5. Joining & Conversion Methods :
